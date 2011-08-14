5096a429c8d79c05f8b2be38e171504d13ce5eed
Alex Clark <aclark@aclark.net>
2011-08-14 18:38:14 -0400

    On to b3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except SyntaxError:
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0b2'
+version = '1.0b3'
 
 long_description = (
     read('README.rst')

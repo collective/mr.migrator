31544d986d0654faf92048808be47e9d482abb9e
Alex Clark <aclark@aclark.net>
2011-08-18 14:58:54 -0400

    Flake8 fixes

--- mr/migrator/runner/__init__.py
+++ mr/migrator/runner/__init__.py
@@ -1,7 +1,7 @@
 from collective.transmogrifier.transmogrifier import configuration_registry
 from collective.transmogrifier.transmogrifier import Transmogrifier
 from optparse import OptionParser, OptionGroup
-from pkg_resources import resource_string, resource_filename
+from pkg_resources import resource_filename
 
 import Products.GenericSetup
 import logging
@@ -31,7 +31,8 @@ class Context:
 class NoErrorParser(OptionParser):
     def error(self):
         pass
- 
+
+
 def runner(args={}, pipeline=None):
     # Make sure GS ZCML is loaded before we load ours
     load_config("meta.zcml", Products.GenericSetup)
@@ -40,21 +41,19 @@ def runner(args={}, pipeline=None):
     load_config('configure.zcml', mr.migrator)
 
     parser = OptionParser()
-    
+
     parser.add_option("--pipeline", dest="pipeline",
                   help="Transmogrifier pipeline.cfg to use",
-                  metavar="FILE"
-                  )
+                  metavar="FILE")
     parser.add_option("--show-pipeline", dest="showpipeline",
-                      action = "store_true",
-                      help="Show contents of the pipeline"
-                      )
+                      action="store_true",
+                      help="Show contents of the pipeline")
     parser.add_option("--zcml", dest="zcml",
-                      action = "store",
-                      help="Load zcml"
-                      )
+                      action="store",
+                      help="Load zcml")
     # Parse just the pipeline args
-    ispipeline = lambda arg: [a for a in ['--pipeline','--show-pipeline'] if arg.startswith(a)]
+    ispipeline = lambda arg: [
+        a for a in ['--pipeline', '--show-pipeline'] if arg.startswith(a)]
     pargs = [arg for arg in sys.argv[1:] if ispipeline(arg)]
     (options, cargs) = parser.parse_args(pargs)
     if options.pipeline is not None:
@@ -62,7 +61,7 @@ def runner(args={}, pipeline=None):
     elif pipeline is not None:
         config = pipeline
     else:
-        config = resource_filename(__name__,'pipeline.cfg')
+        config = resource_filename(__name__, 'pipeline.cfg')
     cparser = configparser.ConfigParser()
     context = Context()
     try:
@@ -85,52 +84,52 @@ def runner(args={}, pipeline=None):
         cparser.read(config)
 
     fp.close()
-     
-    pipeline = [p.strip() for p in cparser.get('transmogrifier','pipeline').split()]
+
+    pipeline = [
+        p.strip() for p in cparser.get('transmogrifier', 'pipeline').split()]
     for section in pipeline:
         if section == 'transmogrifier':
             continue
-        if cparser.has_option(section,'@doc'):
-            doc = cparser.get(section,'@doc')
+        if cparser.has_option(section, '@doc'):
+            doc = cparser.get(section, '@doc')
         else:
             doc = ''
         group = OptionGroup(parser, section, doc)
-        for key,value in cparser.items(section):
+        for key, value in cparser.items(section):
             if key.startswith('@'):
                 if key == '@doc':
                     continue
-                metavar,_,help = value.partition(': ')
+                metavar, _, help = value.partition(': ')
                 if metavar.upper() == metavar:
                     action = "store"
                 else:
                     action = "store_true"
                     help = value
-                group.add_option("--%s:%s"%(section,key[1:]), action=action,
+                group.add_option("--%s:%s" % (section, key[1:]), action=action,
                                              help=help,
                                              metavar=metavar)
         parser.add_option_group(group)
-    pargs = [arg for arg in sys.argv[1:] if not arg.startswith('--template') ]
+    pargs = [arg for arg in sys.argv[1:] if not arg.startswith('--template')]
     (options, cargs) = parser.parse_args(pargs)
 
-    
     cargs = {}
-    for k,_,v in [a.partition('=') for a in sys.argv[1:]]:
+    for k, _, v in [a.partition('=') for a in sys.argv[1:]]:
         k = k.lstrip('--')
         if ':' in k:
-            part,_,key = k.partition(':')
-            if key.lower()=='debug':
+            part, _, key = k.partition(':')
+            if key.lower() == 'debug':
                 logger = logging.getLogger(part)
                 logger.setLevel(logging.DEBUG)
             else:
                 section = cargs.setdefault(part, {})
                 if key in section:
-                    section[key] = '%s\n%s' % (section[key],v)
+                    section[key] = '%s\n%s' % (section[key], v)
                 else:
                     section[key] = v
         else:
             pass
             #cargs[k] = v
-    for k,v in cargs.items():
+    for k, v in cargs.items():
         args.setdefault(k, {}).update(v)
 
     if options.showpipeline:
@@ -148,11 +147,11 @@ def runner(args={}, pipeline=None):
     transmogrifier = Transmogrifier(context)
     overrides = {}
     if type(args) == type(''):
-      for arg in args:
-        section,keyvalue = arg.split(':',1)
-        key,value = keyvalue.split('=',1)
-        overrides.setdefault('section',{})[key] = value
+        for arg in args:
+            section, keyvalue = arg.split(':', 1)
+            key, value = keyvalue.split('=', 1)
+            overrides.setdefault('section', {})[key] = value
     else:
         overrides = args
-        
+
     transmogrifier(pipelineid, **overrides)

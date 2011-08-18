10e61c06dd84552620e0b4cbdab505df7541a52c
Alex Clark <aclark@aclark.net>
2011-08-18 13:58:57 -0400

    Bump

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except SyntaxError:
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0b4'
+version = '1.0b5'
 
 long_description = (
     read('README.rst')

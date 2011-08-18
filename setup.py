27ef6cd0ff14cff293da98245476bec38c3aa18a
Alex Clark <aclark@aclark.net>
2011-08-18 14:01:57 -0400

    Flake8 fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains mr.migrator
 from setuptools import setup, find_packages
 import os
 
-install_requires=[
+install_requires = [
     'collective.transmogrifier',
     'setuptools',
     'z3c.autoinclude',
@@ -15,10 +15,12 @@ try:
     # If we are using Python 2.5 or greater we can require configparser
     eval("1 if True else 2")  # http://stackoverflow.com/questions/446052
     install_requires.append('configparser')
-    install_requires.append('zope.app.component')  # BBB Only needed in Plone >= 4?
+    install_requires.append('zope.app.component')  # BBB Only needed in
+                                                   # Plone >= 4?
 except SyntaxError:
     # If we are using Python 2.4 or lower we cannot require configparser
     pass
+
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
@@ -28,33 +30,31 @@ version = '1.0b5'
 long_description = (
     read('README.rst')
     + '\n' +
-    'Contributors\n' 
+    'Contributors\n'
     '************\n'
     + '\n' +
     read('CONTRIBUTORS.txt')
     + '\n' +
     'Change history\n'
     '**************\n'
-    + '\n' + 
+    + '\n' +
     read('CHANGES.txt')
     + '\n' +
    'Download\n'
-    '********\n'
-    )
+    '********\n')
 entry_point = ''
 entry_points = {"zc.buildout": ["default = mr.migrator.recipe:Recipe"],
                 'console_scripts': ['migrate = mr.migrator.runner:runner'],
                 "z3c.autoinclude.plugin": ["target = plone"],
-                "z3c.autoinclude.plugin":['target = transmogrify'],
+                "z3c.autoinclude.plugin": ['target = transmogrify'],
                 }
 
-tests_require=['zope.testing', 'zc.buildout']
+tests_require = ['zope.testing', 'zc.buildout']
 
 setup(name='mr.migrator',
       version=version,
       description="Drive-by transmogrifiction made easy!",
       long_description=long_description,
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         'Framework :: Buildout',
         'Intended Audience :: Developers',
@@ -73,6 +73,6 @@ setup(name='mr.migrator',
       install_requires=install_requires,
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
-      test_suite = 'mr.migrator.recipe.tests.test_docs.test_suite',
+      test_suite='mr.migrator.recipe.tests.test_docs.test_suite',
       entry_points=entry_points,
       )
